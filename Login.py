a3c71d81bad8521fa59b9f7b1817101f3d211ca3
Jose Miguel Pérez Sánchez <contacto.tk4@gmail.com>
2023-03-11 20:53:33 -0600

    Update Login.py

--- Login.py
+++ Login.py
@@ -17,30 +17,6 @@ def login(usuario,passw):
 usuario=input('Usuario: ')
 passw = input('Contraseña: ')
  
-
-if login(usuario,passw)==1:
-    print('Bienvenido ',usuario)
-else: 
-=======
-#declaracion de variables
-Usuario = ('Miguel','Diego','Alex')
-Contraseña = ('1234')
- 
-#declaracion de funciones
-def login(usuario,passw):
-    if usuario in Usuario:
-        if passw in Contraseña:
-            return 1
-
-        else:
-            print("\n\tEsta no es la contraseña...\n")
-    else:
-        return 2
- #inicializacion de procesos
-usuario=input('Usuario: ')
-passw = input('Contraseña: ')
- 
-
 if login(usuario,passw)==1:
     print('Bienvenido ',usuario)
 else: 
